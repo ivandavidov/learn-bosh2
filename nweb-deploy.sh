90e4db9af092721667961446d1c1ac652d9bbe15
Ivan Davidov <davidov.i@gmail.com>
2017-07-16 00:00:12 +0300

    Updates and improvements.

--- nweb-deploy.sh
+++ nweb-deploy.sh
@@ -20,13 +20,14 @@ if [ "`bosh stemcells | grep ubuntu-trusty`" = "" ]
 then
   if [ ! -e stemcell-ubuntu-trusty.tgz ]
   then
-    wget -O stemcell-ubuntu-trusty.tgz https://bosh.io/d/stemcells/bosh-warden-boshlite-ubuntu-trusty-go_agent
+    wget -O stemcell-ubuntu-trusty.tgz \
+      https://bosh.io/d/stemcells/bosh-warden-boshlite-ubuntu-trusty-go_agent
   fi
 
   bosh -n upload-stemcell stemcell-ubuntu-trusty.tgz
 fi
 
-bosh -n create-release
+bosh -n create-release --force
 bosh -n upload-release
 bosh -n -d nweb deploy manifest.yml
 
